bbbf6fff1465f4a0cd2eceffa8108b7b44005dd9
Hanada <33758411+HanadaLee@users.noreply.github.com>
2019-07-24 17:04:43 +0800

    Add a extra repo to install ajenti on ubuntu 18.04

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -3,12 +3,17 @@ if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-
 echo ':: Installing repo key'
 wget http://repo.ajenti.org/debian/key -O- | apt-key add -
 
 echo ':: Adding repo entry'
 echo "deb http://repo.ajenti.org/debian main main ubuntu" > /etc/apt/sources.list.d/ajenti.list
+
+echo ':: Installing extra repo key for ubuntu 18.04'
+wget https://repo.hanada.info/ubuntu/key -O- | apt-key add -
+
+echo ':: Adding extra repo entry'
+echo "deb https://repo.hanada.info/ubuntu bionic-compatible muitiverse" >> /etc/apt/sources.list.d/ajenti.list
 
 echo ':: Updating lists'
 apt-get update
